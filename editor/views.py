4b2f7c57ab09602190a8bc428f205fb748c8e960
Luke S <k20051009@kcl.ac.uk>
2023-01-31 01:00:13 +0000

    add: Update create view to accept POST request

--- editor/views.py
+++ editor/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
 from .forms import ParagraphForm
+from .models import Paragraph
 
 def home(request):
     return render(request, 'home.html')
 
 def create(request):
+    if request.method == 'POST':
+        form = ParagraphForm(request.POST)
+        # Check if given text is valid
+        if form.is_valid():
+            text = form.cleaned_data.get('text')
+            paragraph = Paragraph.objects.create(text=text)
+            return redirect('browse')
+        messages.add_message(request, messages.ERROR, "Please enter a valid paragraph")
+
+
+    # Otherwise create simple unbound form:
     form = ParagraphForm()
     return render(request, 'create.html', {'form':form})
 
