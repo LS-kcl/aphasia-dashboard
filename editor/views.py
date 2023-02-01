c1272dc4f9cbf8c1e069da38d021f0c28a0199d9
Luke S <k20051009@kcl.ac.uk>
2023-02-01 00:38:51 +0000

    fix: Fix incorrect delimiting of paragraphs leading to empty sentences

--- editor/views.py
+++ editor/views.py
@@ -18,10 +18,14 @@ def create(request):
             # Create sentences from this text, and add to a set
             ## Delimit text
             sentences = text.split(".")
+            ## Strip leading and trailing spaces
+            stripped_sentences = [sentence.strip() for sentence in sentences]
+            ## Filter empty sentences out
+            filtered_sentences = [sentence for sentence in stripped_sentences if sentence]
             ## Create set
             set = Set.objects.create()
             ## For delimited text:
-            for sentence in sentences:
+            for sentence in filtered_sentences:
                 ## Create sentence
                 Sentence.objects.create(parent_set=set,text=sentence)
                 ## Add to set
@@ -35,7 +39,28 @@ def create(request):
 
 def pick_images(request, id):
     if request.method == 'POST':
-        return redirect('browse')
+        print(request.POST)
+        # Create a form for each returned value so we can validate them:
+        forms_submissions = request.POST.getlist('text')
+        print(forms_submissions)
+        # Create new forms with the the current set as the parent
+        current_set = Set.objects.get(pk=id)
+        forms = [SentenceForm({'text':sentence,'parent_set':current_set}) for sentence in forms_submissions]
+        print(forms)
+
+        # Check that all forms are valid
+        is_valid = [form.is_valid() for form in forms]
+        print(is_valid)
+        if all(is_valid):
+            print("Forms are all valid")
+            # If all forms are valid, save all forms, and redirect to new page
+            for form in forms:
+                form.save()
+            return redirect('view_page', id=id)
+        
+        # If not valid, return error messages
+        messages.add_message(request, messages.ERROR, "All sentences must be valid")
+        # return redirect('browse')
 
     set = Set.objects.get(pk=id)
     sentences_queryset = Sentence.objects.filter(parent_set=id).values()
