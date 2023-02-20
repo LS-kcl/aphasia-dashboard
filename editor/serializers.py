4006a25886e3af51b37d60683cb2ef93c652319a
Luke <56962839+Luke-Sanmoogan@users.noreply.github.com>
2023-02-20 13:46:43 +0000

    add: Add two serializers for the Set object
    Add SetSerializer to return all fields in Set object
    Add SetAndSentenceSerializer object to return the set object with child Sentence object data

--- editor/serializers.py
+++ editor/serializers.py
@@ -1,4 +1,4 @@
-from .models import Paragraph
+from .models import Paragraph, Set, Sentence
 from rest_framework import serializers
 
 class ParagraphSerializer(serializers.ModelSerializer):
@@ -9,4 +9,26 @@ class ParagraphSerializer(serializers.ModelSerializer):
             'text'
         ])
 
+class SetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Set
+        fields='__all__'
 
+class SetPlusSentencesSerializer(serializers.ModelSerializer):
+    # Add data of child sentences as a field using a method
+    # See: https://www.django-rest-framework.org/api-guide/fields/#serializermethodfield
+    child_sentences = serializers.SerializerMethodField()
+    
+    class Meta:
+        model=Set
+        fields='__all__'
+
+    def get_child_sentences(self, obj):
+        # Get all child sentences as queryset
+        queryset = Sentence.objects.filter(parent_set=obj.id) 
+
+        # Serialize this queryset data
+        serializer = ParagraphSerializer(queryset, many=True)
+
+        # Return the serialized data
+        return serializer.data
