ae706074df29ca9ab29de0daa54adc5c4fe6e328
Luke S <k20051009@kcl.ac.uk>
2023-03-08 14:10:03 +0000

    refactor: Move pages to new src/pages directory

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import PageList from './components/SetList';
-import SetAdd from './components/SetAdd';
-import WriteParagraph from './components/WriteParagraph'
+import PageList from './pages/SetList';
+import SetAdd from './pages/SetAdd';
+import WriteParagraph from './pages/WriteParagraph'
 
 function App() {
   return (
     <div className="App">
       <Router>
           <Routes>
-              <Route path='/' element={<PageList/>}/>
-              <Route path='/browse' element={<SetAdd/>}/>
+              <Route path='/' element={<SetAdd/>}/>
+              <Route path='/browse' element={<PageList/>}/>
               <Route path='/write_paragraph' element={<WriteParagraph/>}/>
           </Routes>
       </Router>
