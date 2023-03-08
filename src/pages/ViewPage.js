cd9f226ed500b810e4d86c59a318769f9ff2462b
Luke S <k20051009@kcl.ac.uk>
2023-03-08 22:35:04 +0000

    refactor: Modify ViewPage export to work with url parameters

--- src/pages/ViewPage.js
+++ src/pages/ViewPage.js
@@ -2,15 +2,21 @@ import React from "react";
 import axios from "axios";
 import SentenceHelper from "../helpers/SentenceHelper";
 import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
-export default class ViewPage extends React.Component {
+class ViewPage extends React.Component {
     state = {
         parentid: 0,
         sentences: []
     }
 
     componentDidMount() {
-        axios.get('api/view_set/100')
+        // Get id value from URL to make request
+        let { pageid } = this.props.params;
+        console.log(pageid)
+
+        // Make request for page based on pageid
+        axios.get('/api/view_set/' + pageid)
             .then(res =>{
                 const responsejson = res.data;
                 this.setState({parentid: 0, sentences: responsejson.child_sentences })
@@ -38,3 +44,11 @@ export default class ViewPage extends React.Component {
         )
     }
 }
+
+// See: https://stackoverflow.com/questions/58548767/react-router-dom-useparams-inside-class-component
+function withParams(Component) {
+    return props => <Component {...props} params={useParams()} />;
+}
+
+// Helper function required to pass params
+export default withParams(ViewPage)
