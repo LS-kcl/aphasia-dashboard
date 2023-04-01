296b7a7b5daf2968542ce6a9162618482ad37cdc
Luke S <k20051009@kcl.ac.uk>
2023-04-02 00:05:02 +0100

    refactor: Modify SetAdd to take title and text in one input

--- src/pages/SetAdd.js
+++ src/pages/SetAdd.js
@@ -1,15 +1,31 @@
 import React from "react";
 import axios from "axios";
+import { Remarkable } from "remarkable";
 import { Link } from "react-router-dom";
+import { Col, Row } from "react-bootstrap";
 axios.defaults.withCredentials = true;
 
 export default class SetAdd extends React.Component {
-    state = {
-        title: ''
+    constructor(props){
+        super(props);
+        this.markdown = new Remarkable();
+        this.handleChangeText = this.handleChangeText.bind(this);
+        this.state = { 
+            title: '',
+            text: ''
+        };
     }
 
-    handleChange = event => {
+    getRawMarkup() {
+        return { __html: this.markdown.render(this.state.text) }
+    }
+
+    handleChangeTitle = event => {
         this.setState({ title: event.target.value })
+    }
+
+    handleChangeText = event => {
+        this.setState({ text: event.target.value })
     }
 
     handleSubmit = event => {
@@ -17,8 +33,9 @@ export default class SetAdd extends React.Component {
 
         // Creating a json to send using axios
         const title = this.state.title
+        const text = this.state.text
 
-        axios.post(`/api/create_set`, { title })
+        axios.post(`/api/create_set`, { title, text })
             .then(res => {
                 console.log(res);
                 console.log(res.data);
@@ -31,8 +48,26 @@ export default class SetAdd extends React.Component {
                 <form onSubmit={this.handleSubmit}>
                     <label>
                         Set Title:
-                        <input type="text" name="title" onChange={this.handleChange}/>
+                        <input type="text" name="title" onChange={this.handleChangeTitle}/>
                     </label>
+                    <Row>
+                        <Col>
+                            <h3> Enter your text here: </h3>
+                            <textarea 
+                                id="userInput"
+                                name="text"
+                                onChange={this.handleChangeText}
+                                defaultValue={this.state.value}
+                            />
+                        </Col>
+                        <Col>
+                            <h3> Text preview: </h3>
+                            <div
+                                className="output"
+                                dangerouslySetInnerHTML={this.getRawMarkup()}
+                            />
+                        </Col>
+                    </Row>
                     <button type="submit">Create Set</button>
                 </form>
 
